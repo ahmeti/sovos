7d71e6d5b15b806b83d73e1ec60700667097f262
Ahmet Imamoglu <ahmet363@gmail.com>
2025-01-23 12:55:48 +0700

    Invoice/GetInvoiceView için parametre detayları ilave edildi.

--- src/Invoice/GetInvoiceView.php
+++ src/Invoice/GetInvoiceView.php
@@ -4,6 +4,14 @@ namespace Ahmeti\Sovos\Invoice;
 
 class GetInvoiceView
 {
+    /**
+     * @param string|null $UUID Fatura ETTN
+     * @param string|null $CustInvID Müşteri Fatura ID
+     * @param string|null $Identifier Faturanın gönderici ya da alıcısına ait Gönderici Birim (GB) ya da Posta Kutusu (PK) numarası
+     * @param string|null $VKN_TCKN Faturanın gönderici ya da alıcısına ait VKN/TCKN
+     * @param string|null $Type Gelen/Gönderilen Fatura (OUTBOUND, INBOUND)
+     * @param string|null $DocType Doküman Türü (HTML, PDF, XSLT, HTML_DEFAULT, PDF_DEFAULT)
+     */
     public function __construct(
         public string $soapAction = 'getInvoiceView',
         public string $methodName = 'getInvoiceViewRequest',
